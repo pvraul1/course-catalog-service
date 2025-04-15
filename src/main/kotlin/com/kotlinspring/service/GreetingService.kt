4023540b533ea13655315b24bfe241d32535c0d7
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-04-15 08:20:09 +0200

    54. Setting up different profiles in Spring Boot

--- src/main/kotlin/com/kotlinspring/service/GreetingService.kt
+++ src/main/kotlin/com/kotlinspring/service/GreetingService.kt
@@ -1,10 +1,14 @@
 package com.kotlinspring.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class GreetingService {
 
-    fun retrieveGreeting(name: String) =  "Hello $name!"
+    @Value("\${message}")
+    lateinit var message: String
+
+    fun retrieveGreeting(name: String) =  "$name, $message"
 
 }
