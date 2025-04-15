282c7c2703c35e8296be22d44784e977529757a3
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-04-15 17:37:52 +0200

    62. Integration test for the POST Endpoint using JUnit5

---
+++ src/test/intg/com/kotlinspring/controller/CourseControllerIntgTest.kt
@@ -0,0 +1,39 @@
+package com.kotlinspring.controller
+
+import com.kotlinspring.dto.CourseDTO
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+import kotlin.test.assertTrue
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+class CourseControllerIntgTest {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun addCourse() {
+        val courseDTO = CourseDTO(null, "Build Restful APIs using SpringBoot and Kotlin", "Development")
+
+        val savedCourseDTO = webTestClient.post()
+            .uri("/v1/courses")
+            .bodyValue(courseDTO)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(CourseDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        assertTrue {
+            savedCourseDTO!!.id != null
+        }
+
+    }
+
+}
