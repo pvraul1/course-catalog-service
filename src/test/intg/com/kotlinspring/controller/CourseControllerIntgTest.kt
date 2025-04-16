b26005cd6ce8d08d2147e0a4c857a114bd3de531
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-04-16 08:22:03 +0200

    64. Integration test for the GET endpoint to retrieve all the courses

--- src/test/intg/com/kotlinspring/controller/CourseControllerIntgTest.kt
+++ src/test/intg/com/kotlinspring/controller/CourseControllerIntgTest.kt
@@ -1,6 +1,9 @@
 package com.kotlinspring.controller
 
 import com.kotlinspring.dto.CourseDTO
+import com.kotlinspring.repository.CourseRepository
+import com.kotlinspring.util.courseEntityList
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
@@ -16,6 +19,16 @@ class CourseControllerIntgTest {
 
     @Autowired
     lateinit var webTestClient: WebTestClient
+
+    @Autowired
+    lateinit var courseRepository: CourseRepository
+
+    @BeforeEach
+    fun setUp() {
+        courseRepository.deleteAll()
+        val courses = courseEntityList()
+        courseRepository.saveAll(courses)
+    }
 
     @Test
     fun addCourse() {
@@ -34,6 +47,23 @@ class CourseControllerIntgTest {
             savedCourseDTO!!.id != null
         }
 
+    }
+
+    @Test
+    fun retrieveAllCourses() {
+        val courses = webTestClient.get()
+            .uri("/v1/courses")
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList(CourseDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        println("courses: $courses")
+
+        assertTrue {
+            courses!!.size == 3
+        }
     }
 
 }
---
+++ src/test/intg/com/kotlinspring/util/TestUtil.kt
@@ -0,0 +1,9 @@
+package com.kotlinspring.util
+
+import com.kotlinspring.entity.Course
+
+fun courseEntityList() = listOf(
+    Course(null, "Build Restful APIs using SpringBoot and Kotlin", "Development"),
+    Course(null, "Build Reactive Microservices using Spring WebFlux/SpringBoot", "Development"),
+    Course(null, "Wiremock for Java Developers", "Development")
+)
