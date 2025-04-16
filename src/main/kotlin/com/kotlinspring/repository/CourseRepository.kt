93810e18b8366345baf0eaeefc9e2e77c68c18f7
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-04-16 20:29:42 +0200

    76. Retrieve Courses By Name using JPA Query Creation Function

--- src/main/kotlin/com/kotlinspring/repository/CourseRepository.kt
+++ src/main/kotlin/com/kotlinspring/repository/CourseRepository.kt
@@ -5,4 +5,6 @@ import org.springframework.data.repository.CrudRepository
 
 interface CourseRepository : CrudRepository<Course, Int> {
 
+    fun findByNameContaining(courseName: String): List<Course>
+
 }
---
+++ src/test/intg/com/kotlinspring/repository/CourseRepositoryIntgTest.kt
@@ -0,0 +1,38 @@
+package com.kotlinspring.repository
+
+import com.kotlinspring.util.courseEntityList
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.ActiveProfiles
+
+@DataJpaTest
+@ActiveProfiles("test")
+class CourseRepositoryIntgTest {
+
+    @Autowired
+    lateinit var courseRepository: CourseRepository
+
+    @BeforeEach
+    fun setUp() {
+        courseRepository.deleteAll()
+        val courses = courseEntityList()
+        courseRepository.saveAll(courses)
+    }
+
+    @Test
+    fun findByNameContaining() {
+        // Given
+        val courseName = "SpringBoot"
+        val expectedCourseCount = 2
+
+        // When
+        val courses = courseRepository.findByNameContaining(courseName)
+        println("courses: $courses")
+
+        // Then
+        assert(courses.size == expectedCourseCount)
+    }
+
+}
