85df62ed931e714b65639b41e06a3220c67565de
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-04-17 14:19:18 +0200

    Assigment 2: Integration Test InstructorController POST Endpoint

---
+++ src/test/intg/com/kotlinspring/controller/InstructorControllerIntgTest.kt
@@ -0,0 +1,43 @@
+package com.kotlinspring.controller
+
+import com.kotlinspring.dto.InstructorDTO
+import com.kotlinspring.repository.InstructorRepository
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+class InstructorControllerIntgTest {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @Autowired
+    lateinit var instructorRepository: InstructorRepository
+
+    @Test
+    fun addInstructor() {
+        val instructorDTO = InstructorDTO(null, "John Doe")
+
+        val savedInstructorDTO = webTestClient.post()
+            .uri("/v1/instructors")
+            .bodyValue(instructorDTO)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(InstructorDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        assertTrue {
+            savedInstructorDTO!!.id != null
+        }
+    }
+
+}
