508d9127b02a3beb888330e23fcefee0a96a0b7a
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-04-17 09:28:55 +0200

    78. Testing Multiple sets of Data using @Parameterized test

--- src/test/intg/com/kotlinspring/repository/CourseRepositoryIntgTest.kt
+++ src/test/intg/com/kotlinspring/repository/CourseRepositoryIntgTest.kt
@@ -3,9 +3,13 @@ package com.kotlinspring.repository
 import com.kotlinspring.util.courseEntityList
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import java.util.stream.Stream
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -48,4 +52,26 @@ class CourseRepositoryIntgTest {
         // Then
         assert(courses.size == expectedCourseCount)
     }
+
+    @ParameterizedTest
+    @MethodSource("courseAndSize")
+    fun findCoursesByName_approach2(courseName: String, expectedCourseCount: Int) {
+        // When
+        val courses = courseRepository.findByNameContaining(courseName)
+        println("courses: $courses")
+
+        // Then
+        assert(courses.size == expectedCourseCount)
+    }
+
+    companion object {
+        @JvmStatic
+        fun courseAndSize(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("SpringBoot", 2),
+                Arguments.of("Wiremock", 1)
+            )
+        }
+    }
+
 }
